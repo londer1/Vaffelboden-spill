5c0d587a31459a15444790a87822f75ce7d131f4
londer1 <148044380+londer1@users.noreply.github.com>
2024-01-15 13:51:03 +0100

    Fix :3

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ let penger = 100;
 let kostnad = 5;
 let pris = 10;
 let varerPaLager = 0;
+let button = document.querySelector(".lageKnapp");
+// let num = 5;
 
 function lagVaffel() {
-    varerPaLager++
-    document.getElementById("varerPaLager").textContent = varerPaLager;
+    // Deaktiver
+    button.disabled = true;
+
+    setTimeout(function() {
+      // Aktiverer
+    button.disabled = false;
+    }, 3000);
+
+    varerPaLager++;
+    document.getElementById("viserVarebeholdning").textContent = varerPaLager + " varer på lager";
+
+    penger --;
+    document.getElementById("viserAntallKroner").textContent = penger + " antall kroner"
 }
