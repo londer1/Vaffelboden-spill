e68b89902c7a3953118f7a13acde4f13ae1fb7ab
londer1 <148044380+londer1@users.noreply.github.com>
2024-01-15 11:05:33 +0100

    startv2

---
+++ script.js
@@ -0,0 +1,9 @@
+let penger = 100;
+let kostnad = 5;
+let pris = 10;
+let varerPaLager = 0;
+
+function lagVaffel() {
+    varerPaLager++
+    document.getElementById("varerPaLager").textContent = varerPaLager;
+}
